20dfb9a9e3ddbb4e6f7536129562bee2bc671db2
Emanuele-Zanni <emazanni2022@gmail.com>
2024-10-06 09:07:54 -0300

    Agregadas Tags a los proyectos

---
+++ src/db/TechIcons.js
@@ -0,0 +1,160 @@
+import Javascript from "../icons/tech/langs/Javascript.astro";
+import Typescript from "../icons/tech/langs/Typescript.astro";
+import Css from "../icons/tech/langs/Css.astro";
+import Html5 from "../icons/tech/langs/Html5.astro";
+import Java from "../icons/tech/langs/Java.astro";
+
+import Nest from "../icons/tech/frameworks/NestJS.astro";
+import Astre from "../icons/tech/frameworks/Astre.astro";
+import Bootstrap from "../icons/tech/frameworks/Bootstrap.astro";
+import Tailwind from "../icons/tech/frameworks/Tailwind.astro";
+import Express from "../icons/tech/frameworks/ExpressJS.astro";
+import React from "../icons/tech/frameworks/React.astro";
+
+import PostgreSQL from "../icons/tech/database/PostgreSQL.astro";
+import Mongo from "../icons/tech/database/MongoDB.astro";
+import MySQL from "../icons/tech/database/MySQL.astro";
+
+import Auth0 from "../icons/tech/auth/Auth0.astro";
+
+import Netlify from "../icons/tech/deploy/Netlify.astro";
+
+import Jasmine from "../icons/tech/testing/Jasmine.astro";
+import Jest from "../icons/tech/testing/Jest.astro";
+
+import NodeJS from "../icons/tech/enviroments/NodeJS.astro";
+import Git from "../icons/tech/other/Git.astro";
+import JWT from "../icons/tech/other/JWT.astro";
+import TypeORM from "../icons/tech/other/TypeORM.astro";
+
+import Placeholder from "../icons/tech/other/Placeholder.astro";
+import NextJS from "../icons/tech/frameworks/NextJS.astro";
+
+export const LANGS = [
+  {
+    name: "Java",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Java,
+  },
+  {
+    name: "Javascript",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Javascript,
+  },
+  {
+    name: "Typescript",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Typescript,
+  },
+  {
+    name: "HTML 5",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Html5,
+  },
+  {
+    name: "CSS",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Css,
+  },
+];
+
+export const FRAMEWORKS = [
+  {
+    name: "Nest",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Nest,
+  },
+  {
+    name: "Astro",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Astre,
+  },
+  {
+    name: "Tailwind",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Tailwind,
+  },
+  {
+    name: "Bootstrap",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Bootstrap,
+  },
+  {
+    name: "Express",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Express,
+  },
+  {
+    name: "React",
+    class: "bg-[#3b3b3b] text-white",
+    icon: React,
+  },
+  {
+    name: "Next",
+    class: "bg-[#3b3b3b] text-white",
+    icon: NextJS,
+  },
+];
+
+export const OTHERS = [
+  {
+    name: "PostgresSQL",
+    class: "bg-[#3b3b3b] text-white",
+    icon: PostgreSQL,
+  },
+  {
+    name: "MongoDB",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Mongo,
+  },
+  {
+    name: "MySQL",
+    class: "bg-[#3b3b3b] text-white",
+    icon: MySQL,
+  },
+  {
+    name: "Auth0",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Auth0,
+  },
+  {
+    name: "Netlify",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Netlify,
+  },
+  {
+    name: "Render",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Placeholder,
+  },
+  {
+    name: "NodeJS",
+    class: "bg-[#3b3b3b] text-white",
+    icon: NodeJS,
+  },
+  {
+    name: "Git",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Git,
+  },
+  {
+    name: "JWT",
+    class: "bg-[#3b3b3b] text-white",
+    icon: JWT,
+  },
+  {
+    name: "TypeORM",
+    class: "bg-[#3b3b3b] text-white",
+    icon: TypeORM,
+  },
+  {
+    name: "Jasmine",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Jasmine,
+  },
+  {
+    name: "Jest",
+    class: "bg-[#3b3b3b] text-white",
+    icon: Jest,
+  },
+];
